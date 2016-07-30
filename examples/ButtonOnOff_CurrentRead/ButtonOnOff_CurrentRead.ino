b6b2fcc44003925fa0f6784ad5be66b23eb23334
Jon Watkins <jon.watkins@spellfoundry.com>
2016-07-30 12:43:34 +0100

    Initial Commit - New Examples

---
+++ examples/ButtonOnOff_CurrentRead/ButtonOnOff_CurrentRead.ino
@@ -0,0 +1,148 @@
+// 
+// Simple example showing how to set the Sleepy Pi to wake on button press
+// and then power up the Raspberry Pi. To switch the RPi off press the button
+// again. If the button is held down the Sleepy Pi will cut the power to the
+// RPi regardless of any handshaking.
+// 
+// + Prints out the Current Consumption of the RPi to the Serial Monitor
+//   every 0.5 secs
+//
+
+// **** INCLUDES *****
+#include "SleepyPi2.h"
+#include <Time.h>
+#include <LowPower.h>
+#include <PCF8523.h>
+#include <Wire.h>
+
+
+#define kBUTTON_POWEROFF_TIME_MS   2000
+#define kBUTTON_FORCEOFF_TIME_MS   8000
+
+
+// States
+typedef enum {
+  eWAIT = 0,
+  eBUTTON_PRESSED,
+  eBUTTON_HELD,
+  eBUTTON_RELEASED
+}eBUTTONSTATE;
+
+typedef enum {
+   ePI_OFF = 0,
+   ePI_BOOTING,
+   ePI_ON,
+   ePI_SHUTTING_DOWN
+}ePISTATE;
+
+const int LED_PIN = 13;
+
+volatile bool  buttonPressed = false;
+eBUTTONSTATE   buttonState = eBUTTON_RELEASED;
+ePISTATE       pi_state = ePI_OFF;
+bool state = LOW;
+unsigned long  time, timePress;
+
+void button_isr()
+{
+    // A handler for the Button interrupt.
+    buttonPressed = true;
+}
+
+//void alarm_isr()
+//{
+    // A handler for the Alarm interrupt.
+//}
+
+
+void setup()
+{  
+  // Configure "Standard" LED pin
+  pinMode(LED_PIN, OUTPUT);		
+  digitalWrite(LED_PIN,LOW);		// Switch off LED
+
+  SleepyPi.enablePiPower(false);  
+  SleepyPi.enableExtPower(false);
+  
+   // Allow wake up triggered by button press
+  attachInterrupt(1, button_isr, LOW);    // button pin 
+  
+  // Initialize serial communication:
+  Serial.begin(9600);
+  Serial.println("Start..");
+  delay(50); 
+  
+  SleepyPi.rtcInit(true);
+  
+}
+
+void loop() 
+{
+    bool   pi_running;
+    float  pi_current; 
+  
+    // Enter power down state with ADC and BOD module disabled.
+    // Wake up when wake button is pressed.
+    // Once button is pressed stay awake.
+    pi_running = SleepyPi.checkPiStatus(true);  // Cut Power if we detect Pi not running
+    if(pi_running == false){ 
+//       SleepyPi.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+    }
+    
+    // Button State changed
+    if(buttonPressed == true){
+        detachInterrupt(1);      
+        buttonPressed = false;  
+        switch(buttonState) { 
+          case eBUTTON_RELEASED:
+              // Button pressed           
+              timePress = millis();             
+              pi_running = SleepyPi.checkPiStatus(false);
+              if(pi_running == false){  
+                  // Switch on the Pi
+                  SleepyPi.enablePiPower(true);
+                  SleepyPi.enableExtPower(true);   
+              }          
+              buttonState = eBUTTON_PRESSED;
+              digitalWrite(LED_PIN,HIGH);           
+              attachInterrupt(1, button_isr, HIGH);                    
+              break;
+          case eBUTTON_PRESSED:
+              // Button Released
+              unsigned long buttonTime;
+              time = millis();
+              buttonState = eBUTTON_RELEASED;
+              pi_running = SleepyPi.checkPiStatus(false);
+              if(pi_running == true){
+                  // Check how long we have held button for
+                  buttonTime = time - timePress;
+                  if(buttonTime > kBUTTON_FORCEOFF_TIME_MS){
+                     // Force Pi Off               
+                     SleepyPi.enablePiPower(false);
+                     SleepyPi.enableExtPower(false);         
+                  } else if (buttonTime > kBUTTON_POWEROFF_TIME_MS){
+                      // Start a shutdown
+                      SleepyPi.piShutdown();
+                      SleepyPi.enableExtPower(false);            
+                  } else { 
+                     // Button not held off long - Do nothing
+                  } 
+              } else {
+                  // Pi not running  
+              }
+              digitalWrite(LED_PIN,LOW);            
+              attachInterrupt(1, button_isr, LOW);    // button pin       
+              break;
+           default:
+              break;
+        }                
+    } 
+    else {
+        pi_current = SleepyPi.rpiCurrent();
+        Serial.print("Current: ");
+        Serial.print(pi_current);  
+        Serial.println(" mA");        
+        delay(500);             
+    }
+}
+
---
+++ examples/WakeDuringDay/WakeDuringDay.ino
@@ -0,0 +1,295 @@
+// 
+// Simple example showing how to set the RTC alarm pin to wake up the Arduino.
+// Like an alarm clock the alarm is triggered at a particular time and 
+// then the Arduino wakes up.
+//
+// In this example two times are set: 
+//      Wakeup time = Time to awake
+//      Bedtime     = Time to go back to Sleep
+//
+// 
+// Note: this example doesn't wake up the RPi. For that add:
+//
+//     SleepyPi.enablePiPower(true);  
+//
+// after arduino wakeup. 
+// 
+// To test on the RPi without power cycling and using the Arduino IDE
+// to view the debug messages, fit the Power Jumper or enable
+// self-power
+// 
+// 
+
+// **** INCLUDES *****
+#include "SleepyPi2.h"
+#include <Time.h>
+#include <LowPower.h>
+#include <PCF8523.h>
+#include <Wire.h>
+
+const char *monthName[12] = {
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+}; 
+
+const int LED_PIN = 13;
+
+// Globals
+// ++++++++++++++++++++ Change me ++++++++++++++++++
+uint8_t  WakeUp_StartHour       = 7;   // Hour in 24 hour clock
+uint8_t  WakeUp_StartMinute     = 00;  // Minutes 
+uint8_t  Bedtime_SleepHour      = 20;   // Hour in 24 hour clock
+uint8_t  Bedtime_SleepMinute    = 00;  // Minutes 
+
+// ++++++++++++++++++++ End Change me ++++++++++++++++++
+
+tmElements_t tm;
+bool  pi_awake = false;
+
+void alarm_isr()
+{
+    // Just a handler for the alarm interrupt.
+    // You could do something here
+}
+
+void setup()
+{
+  // Configure "Standard" LED pin
+  pinMode(LED_PIN, OUTPUT);		
+  digitalWrite(LED_PIN,LOW);		// Switch off LED
+  // initialize serial communication: In Arduino IDE use "Serial Monitor"
+  Serial.begin(9600);
+  Serial.println("Starting...");
+  delay(50);
+  
+  SleepyPi.rtcInit(true);
+
+  // Default the clock to the time this was compiled.
+  // Comment out if the clock is set by other means
+  // ...get the date and time the compiler was run
+  if (getDate(__DATE__) && getTime(__TIME__)) {
+      // and configure the RTC with this info
+      SleepyPi.setTime(DateTime(F(__DATE__), F(__TIME__)));
+  }  
+
+  printTimeNow();
+  
+  Serial.print("Alarm Set for: ");
+  Serial.print(WakeUp_StartHour);
+  Serial.print(":");
+  Serial.println(WakeUp_StartMinute);
+
+  Serial.print("Bedtime Set for: ");
+  Serial.print(Bedtime_SleepHour);
+  Serial.print(":");
+  Serial.println(Bedtime_SleepMinute);
+  
+}
+
+void loop() 
+{
+  
+    if(pi_awake == false)
+    {
+        SleepyPi.rtcClearInterrupts();
+      
+        // Allow wake up alarm to trigger interrupt on falling edge.
+        attachInterrupt(0, alarm_isr, FALLING);   // Alarm pin
+    
+        SleepyPi.enableWakeupAlarm(true);
+        
+        // Setup the Alarm Time
+        SleepyPi.setAlarm(WakeUp_StartHour,WakeUp_StartMinute);   // Hours & Minutes i.e. 22:07 on the 24 hour clock         
+        
+        //PrintRTCRegisters();   // for debug
+        delay(500);   // Delay purely for serial print
+                
+        // Enter power down state with ADC and BOD module disabled.
+        // Wake up when wake up pin is low (which occurs when our alarm clock goes off)
+        SleepyPi.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+        
+        // Disable external pin interrupt on wake up pin.
+        detachInterrupt(0);        
+        SleepyPi.ackAlarm();
+        
+        // Do something here
+        // Example: Read sensor, data logging, data transmission.
+        // Just a handler for the pin interrupt.
+        digitalWrite(LED_PIN,HIGH);   // Switch on LED
+        Serial.println("I've Just woken up on the Alarm!");
+        // Print the time
+        printTimeNow();   
+        delay(50);
+        digitalWrite(LED_PIN,LOW);    // Switch off LED
+        
+        // +++++ uncomment to Power the Rpi ++++++
+        // SleepyPi.enablePiPower(true); 
+        pi_awake = true;      
+     }
+     else
+     {
+          // pi awake                
+          SleepyPi.rtcClearInterrupts();
+        
+          // Allow wake up alarm to trigger interrupt on falling edge.
+          attachInterrupt(0, alarm_isr, FALLING);   // Alarm pin
+      
+          SleepyPi.enableWakeupAlarm(true);
+          
+          // Setup the Alarm Time
+          SleepyPi.setAlarm(Bedtime_SleepHour,Bedtime_SleepMinute);   // Hours & Minutes i.e. 22:07 on the 24 hour clock    
+  
+          delay(500); // Delay purely for serial print
+               
+          // Enter power down state with ADC and BOD module disabled.
+          // Wake up when wake up pin is low (which occurs when our alarm clock goes off)
+          SleepyPi.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+          
+          // Disable external pin interrupt on wake up pin.
+          detachInterrupt(0); 
+          SleepyPi.ackAlarm();              
+     
+          // Do something here
+          // Just a handler for the pin interrupt.
+          digitalWrite(LED_PIN,HIGH);   // Switch on LED
+          Serial.println("I've Just gone to bed...zzzzzzzz");
+          // Print the time
+          printTimeNow();   
+          delay(50);
+          digitalWrite(LED_PIN,LOW);    // Switch off LED
+        
+        // ++++++ uncomment to Power-dwon the Rpi ++++++++
+        // SleepyPi.piShutdown();         
+          pi_awake = false;            
+     }
+  
+}
+
+void printTimeNow()
+{
+    // Read the time
+    DateTime now = SleepyPi.readTime();
+    
+    // Print out the time
+    Serial.print("Ok, Time = ");
+    print2digits(now.hour());
+    Serial.write(':');
+    print2digits(now.minute());
+    Serial.write(':');
+    print2digits(now.second());
+    Serial.print(", Date (D/M/Y) = ");
+    Serial.print(now.day());
+    Serial.write('/');
+    Serial.print(now.month()); 
+    Serial.write('/');
+    Serial.print(now.year(), DEC);
+    Serial.println();
+
+    return;
+}
+bool getTime(const char *str)
+{
+  int Hour, Min, Sec;
+
+  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
+  tm.Hour = Hour;
+  tm.Minute = Min;
+  tm.Second = Sec;
+  return true;
+}
+
+bool getDate(const char *str)
+{
+  char Month[12];
+  int Day, Year;
+  uint8_t monthIndex;
+
+  if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) return false;
+  for (monthIndex = 0; monthIndex < 12; monthIndex++) {
+    if (strcmp(Month, monthName[monthIndex]) == 0) break;
+  }
+  if (monthIndex >= 12) return false;
+  tm.Day = Day;
+  tm.Month = monthIndex + 1;
+  tm.Year = CalendarYrToTm(Year);
+  return true;
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
+}
+
+void PrintRTCRegisters(void)
+{
+  
+      // Debug
+      uint8_t reg_value;
+      reg_value = SleepyPi.rtcReadReg(PCF8523_CONTROL_1);
+      Serial.print("Control 1: 0x");
+      Serial.println(reg_value,HEX);
+      reg_value = SleepyPi.rtcReadReg(PCF8523_CONTROL_2);
+      Serial.print("Control 2: 0x");
+      Serial.println(reg_value, HEX);      
+      reg_value = SleepyPi.rtcReadReg(PCF8523_CONTROL_3);
+      Serial.print("Control 3: 0x");
+      Serial.println(reg_value,HEX); 
+      
+      reg_value = SleepyPi.rtcReadReg(PCF8523_SECONDS);
+      Serial.print("Seconds: ");
+      Serial.println(reg_value,HEX);
+      reg_value = SleepyPi.rtcReadReg(PCF8523_MINUTES);
+      Serial.print("Minutes: ");
+      Serial.println(reg_value,HEX);  
+      reg_value = SleepyPi.rtcReadReg(PCF8523_HOURS);
+      Serial.print("Hours: ");
+      Serial.println(reg_value,HEX);  
+      reg_value = SleepyPi.rtcReadReg(PCF8523_DAYS);
+      Serial.print("Days: ");
+      Serial.println(reg_value,HEX);   
+      reg_value = SleepyPi.rtcReadReg(PCF8523_WEEKDAYS);
+      Serial.print("Week Days: ");
+      Serial.println(reg_value,HEX);    
+      reg_value = SleepyPi.rtcReadReg(PCF8523_MONTHS);
+      Serial.print("Months: ");
+      Serial.println(reg_value,HEX);  
+      reg_value = SleepyPi.rtcReadReg(PCF8523_YEARS);
+      Serial.print("Years: ");
+      Serial.println(reg_value,HEX); 
+      
+      reg_value = SleepyPi.rtcReadReg(PCF8523_MINUTE_ALARM);
+      Serial.print("Minute Alarm: ");
+      Serial.println(reg_value,HEX);      
+      reg_value = SleepyPi.rtcReadReg(PCF8523_HOUR_ALARM);
+      Serial.print("Hour Alarm: ");
+      Serial.println(reg_value,HEX);  
+      reg_value = SleepyPi.rtcReadReg(PCF8523_DAY_ALARM);
+      Serial.print("Day Alarm: ");
+      Serial.println(reg_value,HEX);      
+      reg_value = SleepyPi.rtcReadReg(PCF8523_WEEKDAY_ALARM);
+      Serial.print("Weekday Alarm: ");
+      Serial.println(reg_value,HEX); 
+      
+      reg_value = SleepyPi.rtcReadReg(PCF8523_OFFSET);
+      Serial.print("Offset: 0x");
+      Serial.println(reg_value,HEX); 
+      reg_value = SleepyPi.rtcReadReg(PCF8523_TMR_CLKOUT_CTRL);
+      Serial.print("TMR_CLKOUT_CTRL: 0x");
+      Serial.println(reg_value,HEX);  
+      reg_value = SleepyPi.rtcReadReg(PCF8523_TMR_A_FREQ_CTRL);
+      Serial.print("TMR_A_FREQ_CTRL: 0x");
+      Serial.println(reg_value,HEX); 
+      reg_value = SleepyPi.rtcReadReg(PCF8523_TMR_A_REG);
+      Serial.print("TMR_A_REG: 0x");
+      Serial.println(reg_value,HEX);     
+      reg_value = SleepyPi.rtcReadReg(PCF8523_TMR_B_FREQ_CTRL);
+      Serial.print("TMR_B_FREQ_CTRL: 0x");
+      Serial.println(reg_value,HEX);    
+       reg_value = SleepyPi.rtcReadReg(PCF8523_TMR_B_REG);
+      Serial.print("TMR_B_REG: 0x");
+      Serial.println(reg_value,HEX);     
+ 
+}
+
